9ec67004dc110afbc3cbbbde102d39702a9f5ef5
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-27 17:22:05 +0300

    "Added game logic and UI updates to app.js and index.html"

--- app.js
+++ app.js
@@ -1,9 +1,52 @@
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 const app = Vue.createApp({
   data() {
     return {
       playerHealth: 100,
-      monsterHealth: 1000,
+      monsterHealth: 100,
+      currentRound: 0,
     };
+  },
+  computed: {
+    monsterBarStyles() {
+      return { width: this.monsterHealth + "%" };
+    },
+    playerBarStyles() {
+      return { width: this.playerHealth + "%" };
+    },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
+  },
+  methods: {
+    attackMonster() {
+      const attachValue = getRandomValue(5, 12);
+      this.monsterHealth -= attachValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attachValue = getRandomValue(8, 15);
+      this.playerHealth -= attachValue;
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    healPlayer() {
+      this.currentRound++;
+      const healValue = getRandomValue(8, 20);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      this.attackPlayer();
+    },
   },
 });
 
