601484cf5509eb768cdc9eeeadf042cf3cc0eed9
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-27 17:50:02 +0300

    "Added game over logic, winner tracking, and start new game feature"

--- app.js
+++ app.js
@@ -8,20 +8,49 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       currentRound: 0,
+      winner: null,
     };
   },
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: "0%" };
+      }
       return { width: this.monsterHealth + "%" };
     },
     playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: "0%" };
+      }
       return { width: this.playerHealth + "%" };
     },
     mayUseSpecialAttack() {
       return this.currentRound % 3 !== 0;
     },
   },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        this.winner = "Draw";
+      } else if (value <= 0) {
+        this.winner = "monster";
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth < 0) {
+        this.winner = "Draw";
+      } else if (value <= 0) {
+        this.winner = "player";
+      }
+    },
+  },
   methods: {
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.winner = null;
+      this.currentRound = 0;
+    },
     attackMonster() {
       const attachValue = getRandomValue(5, 12);
       this.monsterHealth -= attachValue;
@@ -46,6 +75,9 @@ const app = Vue.createApp({
         this.playerHealth += healValue;
       }
       this.attackPlayer();
+    },
+    surrender() {
+      this.winner = "monster";
     },
   },
 });
